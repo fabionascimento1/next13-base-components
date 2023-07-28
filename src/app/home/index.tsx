42230130ebca0231cfeb6b7853a099680509faa2
Fabio Nascimento <fabionascimento@smartbr.com>
2023-07-28 09:46:41 -0300

    fix: update home page

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,12 +1,7 @@
-import Icon from "@/components/Icon/Icon";
-import { Button } from "@/components/ui/Button/Button";
-
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-50 flex-col gap-4">
-      <Button type="submit" contained="primary" />
-      <Button contained="secondary" size="xs" />
-      <Icon name="home" color="red" />
+      Home
     </div>
   );
 }
